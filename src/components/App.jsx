0fe16f45a267ac7d744bf3316a4538acfa202801
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-06-26 20:37:45 +0530

    Form greets the user after accepting name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,38 @@ import React, { useState } from "react";
 
 function App() {
   const [isWhat, setIsWhat] = useState(false);
+  const [name, setName] = useState("");
+  const [nameToDisplay, setFinalName] = useState("");
+
   function setTrue() {
     setIsWhat(true);
   }
+
   function setFalse() {
     setIsWhat(false);
   }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function setIsDone() {
+    setFinalName(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {nameToDisplay} </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button
         style={{ backgroundColor: isWhat && "black" }}
         onMouseOver={setTrue}
         onMouseOut={setFalse}
+        onClick={setIsDone}
       >
         Submit
       </button>
